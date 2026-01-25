f6f32cca5f0d1b463d004380e6cd4e5b01773678
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-25 19:15:04 +0545

    Setup initial folder structure and JWT auth, env loading, and drf-spectacular documentation in settings

---
+++ src/config/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView, 
+    SpectacularRedocView, 
+    SpectacularSwaggerView
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # 1. Direct path to download the schema (YAML by default)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    
+    # 2. Swagger UI: Interactive documentation
+    # url_name='schema' tells it where to fetch the OpenAPI definition
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    # 3. Redoc: Clean, static documentation
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    
+    # Include your app URLs here
+    # path('api/accounts/', include('apps.accounts.urls')),
+]    
