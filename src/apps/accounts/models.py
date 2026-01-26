f8a6b658643d86f1a3396d6a6877c930b208aa68
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-26 17:39:49 +0545

    add extra fields for username to accept no values

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
         EMPLOYER = "EMPLOYER", "Employer"
         CANDIDATE = "CANDIDATE", "Candidate"
 
-    username=models.CharField(max_length=200, unique=False)
+    username=models.CharField(max_length=200, unique=True, null=True, blank=True)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=Role.choices)
 
