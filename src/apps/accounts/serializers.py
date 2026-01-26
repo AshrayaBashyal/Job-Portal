1f14b675a929424ddf5384edafa2b0dfbaeed55c
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-26 17:48:29 +0545

    refactor(accounts): enhance user serializer with extra_kwargs for security

--- src/apps/accounts/serializers.py
+++ src/apps/accounts/serializers.py
@@ -7,7 +7,20 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["email", "password", "role", "username"]
-
+        extra_kwargs = {
+            'password': {
+                'write_only': True,   
+                'min_length': 6,      
+            },
+            'username': {
+                'required': False,     # Make optional if email is primary
+                'allow_blank': True
+            },
+            'email': {
+                'required': True
+            }
+        }
     def create(self, validated_data):
+        print(f"{validated_data}")
         from apps.accounts.services import register_user
         return register_user(**validated_data)    
