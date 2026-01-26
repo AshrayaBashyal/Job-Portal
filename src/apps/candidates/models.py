054571ef1506d10ec65f6a3fe8c03906f28deb21
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-26 15:00:25 +0545

    add models for employee and candidate profiles and companies

--- src/apps/candidates/models.py
+++ src/apps/candidates/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class CandidateProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="candidate_profile")
+    full_name = models.CharField(max_length=150)
+    skills = models.TextField(blank=True)
+    experience_years = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.full_name
--- src/apps/companies/models.py
+++ src/apps/companies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL    # ('accounts.User')avoids circular import errors because Django resolves the string into a model class later
+
+
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="employer_profile")
+    company_name = models.CharField(max_length=150)
+    email = models.URLField(unique=True)
+
+    def __Str__(self):
+        return self.company_name
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    employer = models.ForeignKey(EmployerProfile, on_delete=models.CASCADE, related_name="companies")
+
+    def __str__(self):
+        return self.name        
