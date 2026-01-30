5c8639746829119021dcf836e8270e3e0b19d1cd
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-30 22:56:55 +0545

    register url for viewing applied jobs ststus

--- src/apps/applications/urls.py
+++ src/apps/applications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ApplyToJobView, JobApplicationsView, UpdateApplicationStatusView
+from .views import ApplyToJobView, JobApplicationsView, UpdateApplicationStatusView, MyApplicationsView
 
 urlpatterns = [
     path("apply/", ApplyToJobView.as_view()),
     path("employer/", JobApplicationsView.as_view()),
     path("update/<int:pk>/", UpdateApplicationStatusView.as_view()),
+    path("my/", MyApplicationsView.as_view()),
 ]
